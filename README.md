fbae95096ca2dc834eb7105b99f26c91a4e70fb3
graindorgeanthony <graindorgeanthony@gmail.com>
2026-01-11 14:43:01 +0100

    exercise_1

---
+++ README.md
@@ -0,0 +1,3 @@
+# LLM Engineering - Master AI and LLMs
+
+Exercises from the course: https://www.udemy.com/course/llm-engineering-master-ai-and-large-language-models
---
+++ week1/exercise_1_llm_call.py
@@ -0,0 +1,38 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Load API key & config.
+load_dotenv(override=True)
+api_key = os.getenv('OPENROUTER_API_KEY')
+
+client = OpenAI(
+  base_url="https://openrouter.ai/api/v1",
+  api_key=api_key,
+)
+
+# Call the LLM
+completion = client.chat.completions.create(
+  model="google/gemini-2.5-flash",
+  messages=[
+    {
+        "role":"system",
+        "content":[{
+            "type":"text",
+            "text":"Answer in a funny way."
+        }]
+    },
+    {
+      "role": "user",
+      "content": [
+        {
+          "type": "text",
+          "text": "What's the capital of France?"
+        }
+      ]
+    }
+  ]
+)
+
+# Print the response
+print(completion.choices[0].message.content)
