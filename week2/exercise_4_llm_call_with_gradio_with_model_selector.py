1d6318bfa48338f48b3145e18f99558834a36eb8
graindorgeanthony <graindorgeanthony@gmail.com>
2026-01-12 14:09:52 +0100

    week2/exercise4

--- week2/exercise_4_llm_call_with_gradio_with_model_selector.py
+++ week2/exercise_4_llm_call_with_gradio_with_model_selector.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import gradio as gr
 
-from llm_engineering.week2.day2 import model_selector
-
 # Load API key & config.
 load_dotenv(override=True)
 api_key = os.getenv('OPENROUTER_API_KEY')
@@ -17,7 +15,7 @@ client = OpenAI(
 
 system_message = "You are an helpfull assistant that responses in markdown, without code blocks."
 
-# Call the LLM
+# Function to call the LLM with streaming
 def streaming_model(prompt, model):
   messages=[
     {"role":"system","content":[{"type":"text","text":system_message}]},
@@ -33,16 +31,22 @@ def streaming_model(prompt, model):
     result += chunk.choices[0].delta.content or ""
     yield result
 
+# Inputs & outputs
 message_input = gr.Textbox(label="Your message:", info="Enter a message that will be sent to a llama 3.2", lines=5)
 model_selector = gr.Dropdown(choices=["llama3.2", "gemma3:4b"])
 message_output = gr.Markdown(label="Response:")
 
+# Gradio interface
 gr.Interface(
   fn=streaming_model,
   title="Send a message to an LLM",
   inputs=[message_input, model_selector],
   outputs=[message_output],
-  examples=["Hello!", "Tell me a joke", "What are the secrets to happiness?"],
+  examples=[
+    ["Hello!", "llama3.2"],
+    ["Tell me a joke", "llama3.2"],
+    ["What are the secrets to happiness?", "llama3.2"],
+  ],
   flagging_mode="never"
 ).launch(
   inbrowser=True
